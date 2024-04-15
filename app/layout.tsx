1f081e75a4e47a40ab05b0f7aa4e8d9acd611ca9
miaslls <me@camilasalles.dev>
2024-04-14 23:11:14 -0300

    feat: initial style defs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import type { Metadata } from "next";
 import "./globals.css";
-
+import type { Metadata } from "next";
+import { Work_Sans, Outfit } from 'next/font/google';
 
 export const metadata: Metadata = {
   title: "MiaEng",
@@ -16,13 +16,25 @@ export const metadata: Metadata = {
   referrer: 'no-referrer',
 };
 
+const work_sans = Work_Sans({
+  subsets: ['latin'],
+  variable: '--font-work_sans',
+  display: 'swap',
+});
+
+const outfit = Outfit({
+  subsets: ['latin'],
+  variable: '--font-outfit',
+  display: 'swap',
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${work_sans.variable} ${outfit.variable}`}>
       <body>{children}</body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,12 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <></>
+    <>
+      <h1>Heading 1</h1>
+      <h2>Heading 2</h2>
+      <h3>Heading 3</h3>
+
+      <p className="default">Lorem ipsum dolor, <a>sit amet</a> consectetur adipisicing elit. Nam hic eius alias dignissimos quas odit quasi ad illo corrupti culpa iste quaerat esse, nihil recusandae voluptatem consequatur facilis. Deleniti, itaque.</p>
+    </>
   );
 }
