79f4cdcd57f2ac420ea9d7311a07d32a34bc9eaa
miaslls <me@camilasalles.dev>
2024-05-02 00:08:50 -0300

    feat(error): `Error` `GlobalError` `NotFound`

---
+++ app/error.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { useEffect } from 'react';
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="error">
+      <div className="error-container">
+        <div className="error-icon">
+          <i className="fi fi-ts-times-hexagon"></i>
+        </div>
+
+        <div className="wrapper">
+          <h2>Something went wrong!</h2>
+          <button className="button-link error-link" onClick={() => reset()}>
+            Try again
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
