6b668648bc3344e6de8a2151529e7ae9a1a14ef6
miaslls <me@camilasalles.dev>
2024-06-08 19:04:41 -0300

    feat: add data - verb flashcard verbs

---
+++ app/_data/verb-flashcards/base.ts
@@ -0,0 +1,36 @@
+import { VerbFlashcards } from ".";
+
+export const base: VerbFlashcards = {
+  run: 'run',
+  eat: 'eat',
+  drink: 'drink',
+  jump: 'jump',
+  sleep: 'sleep',
+  play: 'play',
+  sing: 'sing',
+  dance: 'dance',
+  laugh: 'laugh',
+  cry: 'cry',
+  brush: 'brush',
+  wash: 'wash',
+  study: 'study',
+  read: 'read',
+  write: 'write',
+  draw: 'draw',
+  cook: 'cook',
+  clean: 'clean',
+  watch: 'watch',
+  listen: 'listen',
+  swim: 'swim',
+  make: 'make',
+  bake: 'bake',
+  take: 'take',
+  sit: 'sit',
+  get: 'get',
+  put: 'put',
+  cut: 'cut',
+  tie: 'tie',
+  hug: 'hug'
+}
+
+export const verbBaseArray = Object.keys(base) as (keyof VerbFlashcards)[];
